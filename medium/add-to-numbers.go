6a56c00cf8ca3505477a410780d12893d7aeab0b
Zachary Groff <groff.zackm@gmail.com>
2020-05-21 14:43:01 -0400

    Create add-to-numbers.go

---
+++ medium/add-to-numbers.go
@@ -0,0 +1,40 @@
+/*
+https://leetcode.com/problems/add-two-numbers/submissions/
+*/
+
+func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
+    current := &ListNode{}
+    head := current
+    remainder := 0
+    for {        
+        sum := 0
+        if (l1 != nil) {
+            sum += (*l1).Val
+            l1 = (*l1).Next
+        }
+        if (l2 != nil) {
+            sum += (*l2).Val
+            l2 = (*l2).Next
+        }
+        
+        sum += remainder
+        (*current).Val = sum % 10
+        
+        remainder = sum / 10
+        if (l1 == nil && l2 == nil) {
+            if (remainder > 0) {
+                newNode := &ListNode{
+                    Val: remainder,
+                }
+                (*current).Next = newNode
+            }
+            break
+        } else {
+            newNode := &ListNode{}
+            (*current).Next = newNode
+            current = newNode
+        }
+    }
+
+    return head
+}
